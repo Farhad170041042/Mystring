f62298764fc45806dfa1b25540fdbd949f97f71a
Farhad170041042 <abdullahalfarhad.7626@gmail.com>
2019-02-11 13:17:32 +0600

    MyString.cpp

---
+++ MyString.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class MyString
+{
+public:
+
+  MyString()
+  {
+    size = 100;
+    str = (char*)malloc(sizeof(char) * 100 );
+  }
+
+  MyString(int _size)
+  {
+    size = _size;
+    str = (char*)malloc(sizeof(char) * size);
+  }
+
+  bool set(char *inp)
+  {
+    int len = 0;
+    for ( int i = 0 ; inp[i] != NULL ; i++ )
+    {
+      len++;
+    }
+    if ( len <= size )
+    {
+      str = inp;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  void print()
+  {
+    char* s;
+    s = str;
+    cout << s << endl;
+  }
+
+private:
+  int size;
+  char* str;
+};
